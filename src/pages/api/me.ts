d69cf5811004ffafedf7ad348ec1d25873d6d8d6
Victor Prado <prado.victor1005@gmail.com>
2025-03-27 12:22:06 -0300

    fix: ajustando userContext

--- src/pages/api/me.ts
+++ src/pages/api/me.ts
@@ -1,25 +1,25 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+// import type { NextApiRequest, NextApiResponse } from "next";
 
-import cookie from "cookie";
-import { IUser } from "@/app/types/IUser";
-import { fetchUserData } from "@/lib/api";
+// import cookie from "cookie";
+// import { IUser } from "@/app/types/IUser";
+// import { fetchUserData } from "@/lib/api";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<IUser | { error: string }>
-) {
-  const cookies = cookie.parse(req.headers.cookie || "");
-  const authToken = cookies.authToken;
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse<IUser | { error: string }>
+// ) {
+//   const cookies = cookie.parse(req.headers.cookie || "");
+//   const authToken = cookies.authToken;
 
-  if (!authToken) {
-    return res.status(401).json({ error: "Usuário não autenticado" });
-  }
+//   if (!authToken) {
+//     return res.status(401).json({ error: "Usuário não autenticado" });
+//   }
 
-  const user = await fetchUserData({ uuid_colaborador: authToken });
+//   const user = await fetchUserData({ uuid_colaborador: authToken });
 
-  if (!user) {
-    return res.status(404).json({ error: "Usuário não encontrado" });
-  }
+//   if (!user) {
+//     return res.status(404).json({ error: "Usuário não encontrado" });
+//   }
 
-  return res.status(200).json(user);
-}
+//   return res.status(200).json(user);
+// }
